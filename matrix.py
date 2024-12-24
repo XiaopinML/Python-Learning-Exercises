e080d1a18923d6c309d9c40b1411219d9bd7f6c8
Haji Ali <hajiminxo@163.com>
2024-12-24 20:20:00 +0800

    matrix printing

--- matrix.py
+++ matrix.py
@@ -1,8 +1,6 @@
-print("Multiplication Table:")
-# Outer loop iterates over the rows from 1 to 19 (inclusive)
-for i in range(1,20):
-# Outer loop iterates over the rows from 1 to 19 (inclusive)
-    for j in range(1,20):
-# Outer loop iterates over the rows from 1 to 19 (inclusive)
-        print(f"{i}*{j} = {i*j}\n", end=" ")
-print()
+#Nested loops to iterate over a two-dimensional list
+matrix = [[1,2,4],[3,1,2],[2,3,1]]
+for row in matrix:
+    for element in row:
+        print(element ,end=" ")
+    print()
