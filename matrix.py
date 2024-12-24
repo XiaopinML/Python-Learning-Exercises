2ba7a7063a7791b28c4e37995478d444f88bac33
Haji Ali <hajiminxo@163.com>
2024-12-24 20:08:37 +0800

    done few exercises

---
+++ matrix.py
@@ -0,0 +1,8 @@
+print("Multiplication Table:")
+# Outer loop iterates over the rows from 1 to 19 (inclusive)
+for i in range(1,20):
+# Outer loop iterates over the rows from 1 to 19 (inclusive)
+    for j in range(1,20):
+# Outer loop iterates over the rows from 1 to 19 (inclusive)
+        print(f"{i}*{j} = {i*j}\n", end=" ")
+print()
